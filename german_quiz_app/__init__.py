9b5abf37b4791b5b417cca035a98b14244c74154
Nemanja Radojkovic <nemanjaradojkovic@nemanja-roots.local>
2020-05-04 13:42:45 +0200

    blackened reformatted

--- german_quiz_app/__init__.py
+++ german_quiz_app/__init__.py
@@ -2,24 +2,27 @@ import flask
 import os
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
+dynamodb = boto3.resource("dynamodb", region_name="eu-central-1")
 
-table = dynamodb.Table('german_quiz')
+table = dynamodb.Table("german_quiz")
 
-table.put_item(Item={'phrase_de': 'Vorhersage', 'phrase_en': 'Prediction'})
+table.put_item(Item={"phrase_de": "Vorhersage", "phrase_en": "Prediction"})
 
 app = flask.Flask(__name__)
 
 # Only enable Flask debugging if an env var is set to true
-app.debug = os.environ.get('FLASK_DEBUG') in ['true', 'True']
+app.debug = os.environ.get("FLASK_DEBUG") in ["true", "True"]
 
 # Get application version from env
-app.config['APP_VERSION'] = os.environ.get('APP_VERSION')
+app.config["APP_VERSION"] = os.environ.get("APP_VERSION")
 
 # Get cool new feature flag from env
-app.config['enable_cool_new_feature'] = os.environ.get('ENABLE_COOL_NEW_FEATURE') in ['true', 'True']
+app.config["enable_cool_new_feature"] = os.environ.get("ENABLE_COOL_NEW_FEATURE") in [
+    "true",
+    "True",
+]
 
 from german_quiz_app import views
- 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
--- german_quiz_app/views.py
+++ german_quiz_app/views.py
@@ -1,15 +1,16 @@
-
 import flask
 from german_quiz_app import app
 from datetime import datetime as dt
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     message = "Hello, world!"
-    return flask.render_template('index.html',
-                                  title=message,
-                                  date_today=dt.now().strftime("%Y-%m-%d"),
-                                  flask_debug=app.debug,
-                                  app_version=app.config['APP_VERSION'],
-                                  enable_cool_new_feature=app.config['enable_cool_new_feature'])
+    return flask.render_template(
+        "index.html",
+        title=message,
+        date_today=dt.now().strftime("%Y-%m-%d"),
+        flask_debug=app.debug,
+        app_version=app.config["APP_VERSION"],
+        enable_cool_new_feature=app.config["enable_cool_new_feature"],
+    )
