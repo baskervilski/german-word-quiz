7c8d86d86eb9b8467c4c1dd4e57f0ca5075e6402
Nemanja Radojkovic <nemanjaradojkovic@nemanja-roots.local>
2020-05-10 12:12:14 +0200

    minor tweaks

--- german_quiz_app/__init__.py
+++ german_quiz_app/__init__.py
@@ -4,9 +4,7 @@ import boto3
 import german_quiz_app.config as cfg
 
 dynamodb = boto3.resource("dynamodb", region_name=cfg.AWS_REGION)
-
 table = dynamodb.Table(cfg.DICT_TABLE)
-
 app = flask.Flask(__name__)
 
 # Only enable Flask debugging if an env var is set to true
@@ -18,7 +16,8 @@ app.config['API_ENDPOINT'] = cfg.API_ENDPOINT
 app.config["FLASK_ENDPOINT_HOST"] = cfg.FLASK_ENDPOINT_HOST
 app.config['FLASK_ENDPOINT_PORT'] = cfg.FLASK_ENDPOINT_PORT
 
-from german_quiz_app import views
+from german_quiz_app import views   
 
 if __name__ == "__main__":
+    
     app.run(host=app.config["FLASK_ENDPOINT_HOST"], port=app.config['FLASK_ENDPOINT_PORT'])
--- german_quiz_app/config.py
+++ german_quiz_app/config.py
@@ -9,5 +9,3 @@ AWS_REGION = "eu-central-1"
 FLASK_ENDPOINT_HOST = os.getenv('FLASK_ENDPOINT_HOST', '0.0.0.0')
 FLASK_ENDPOINT_PORT = os.getenv('FLASK_ENDPOINT_PORT', 5000)
 API_ENDPOINT = os.getenv('API_ENDPOINT') # , "germanwordquiz-env.eba-5pjscnfr.eu-central-1.elasticbeanstalk.com")
-
-print(API_ENDPOINT)
