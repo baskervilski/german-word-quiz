a71441a0d0c815e402b41f056419998653ddf0fc
Nemanja Radojkovic <nemanjaradojkovic@nemanja-roots.local>
2020-05-10 11:38:57 +0200

    url bug fix

--- german_quiz_app/config.py
+++ german_quiz_app/config.py
@@ -8,4 +8,4 @@ AWS_REGION = "eu-central-1"
 
 FLASK_ENDPOINT_HOST = os.getenv('FLASK_ENDPOINT_HOST', '0.0.0.0')
 FLASK_ENDPOINT_PORT = os.getenv('FLASK_ENDPOINT_PORT', 5000)
-API_ENDPOINT = os.getenv('API_ENDPOINT', "http://germanwordquiz-env.eba-5pjscnfr.eu-central-1.elasticbeanstalk.com")
+API_ENDPOINT = os.getenv('API_ENDPOINT', "germanwordquiz-env.eba-5pjscnfr.eu-central-1.elasticbeanstalk.com")
