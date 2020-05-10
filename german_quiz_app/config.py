1ceeb0bbcd375afef0259c3100f11cb49619d53e
Nemanja Radojkovic <nemanjaradojkovic@nemanja-roots.local>
2020-05-10 11:55:15 +0200

    added debug print

--- german_quiz_app/config.py
+++ german_quiz_app/config.py
@@ -9,3 +9,5 @@ AWS_REGION = "eu-central-1"
 FLASK_ENDPOINT_HOST = os.getenv('FLASK_ENDPOINT_HOST', '0.0.0.0')
 FLASK_ENDPOINT_PORT = os.getenv('FLASK_ENDPOINT_PORT', 5000)
 API_ENDPOINT = os.getenv('API_ENDPOINT') # , "germanwordquiz-env.eba-5pjscnfr.eu-central-1.elasticbeanstalk.com")
+
+print(API_ENDPOINT)
